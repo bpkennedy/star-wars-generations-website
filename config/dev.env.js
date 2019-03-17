f5b4282f4310e29c2f2f726a8bbf7a377dcd09fb
bpkennedy <bpkennedy@gmail.com>
2019-03-17 00:09:23 -0500

    Update to new Star Wars Generations name in places and enable .env file

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,9 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  COSMIC_BUCKET: `"${process.env.COSMIC_BUCKET || 'star-wars-generations-website'}"`,
+  COSMIC_READ_KEY: `"${process.env.COSMIC_READ_KEY}"`,
+  COSMIC_WRITE_KEY: `"${process.env.COSMIC_WRITE_KEY}"`,
+  BLOG_TITLE: `"${process.env.BLOG_TITLE || 'Star Wars Generations'}"`,
 })
